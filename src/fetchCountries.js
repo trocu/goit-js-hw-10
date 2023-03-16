1a00e449007f3874fb4ed64d1cd4e2fd944147cc
Mariusz Trocewicz <throcu@gmail.com>
2023-03-17 00:35:00 +0100

    Add named export

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,14 @@
+// https://restcountries.com/v3.1/all
 
+export const fetchCountries = name => {
+  const paramString = `fields=name&fields=capital&fields=population&fields=flags&fields=languages`;
+  const searchParams = new URLSearchParams(paramString);
+  const url = `https://restcountries.com/v3.1/name/${name}?${searchParams}`;
+  console.log(url);
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
