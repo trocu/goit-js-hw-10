bda6fd47b0a1f92e3b726dc950c4b41f4e01e8bc
Mariusz Trocewicz <throcu@gmail.com>
2023-03-17 23:42:36 +0100

    Fetching WIP

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,10 @@
 // https://restcountries.com/v3.1/all
+import { searchBox } from '.';
 
 export const fetchCountries = name => {
   const paramString = `fields=name&fields=capital&fields=population&fields=flags&fields=languages`;
   const searchParams = new URLSearchParams(paramString);
-  const url = `https://restcountries.com/v3.1/name/${name}?${searchParams}`;
-  console.log(url);
+  const url = `https://restcountries.com/v3.1/name/${searchBox.value.trim()}?${searchParams}`;
   return fetch(url).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 
@@ -5,12 +6,15 @@ const DEBOUNCE_DELAY = 300;
 
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
-const searchBox = document.querySelector('#search-box');
+export const searchBox = document.querySelector('#search-box');
 
-searchBox.addEventListener('input', () => {
-  fetchCountries('poland')
-    .then(name => console.log(name))
-    .catch(error => console.log(error));
-});
+searchBox.addEventListener(
+  'input',
+  debounce(() => {
+    fetchCountries()
+      .then(name => console.log(name))
+      .catch(error => console.log(error));
+  }, DEBOUNCE_DELAY)
+);
 
-function renderCountryList(name) {}
+// function renderCountryList(name) {}
